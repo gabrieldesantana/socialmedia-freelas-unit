c029a42870b3b71eb589b7532d6bfea612f4e203
gabrieldesantana <gabrieldesantana.contato@gmail.com>
2024-06-02 10:00:48 -0300

    Ajusta navbar e muda tela home cliente e freelancer

---
+++ SocialMediaFreelas.App/Pages/Vagas/IndexByUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SocialMediaFreelas.Pages.Vagas
+{
+    public class IndexByUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
